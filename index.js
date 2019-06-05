84f09fc54ef0b23ea2180f6c4ae72e89d595d7ea
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2019-06-05 12:00:33 +0800

    Feature - used rss feed instead of json since data is incomplete. Actual content of post is not included on medium json api

--- index.js
+++ index.js
@@ -1,51 +1,20 @@
-const https = require('https');
+const Feed = require('rss-to-json')
 
-const fail = (status, error, reject, callback) => {
-  const result = { status, error };
-  callback instanceof Function && callback(result, result);
-  reject(result);
-};
-
-module.exports = (endpoint = '/', callback) => {
+module.exports = async (endpoint = '/', callback) => {
   if (endpoint.charAt(0) !== '/') {
-    endpoint = '/' + endpoint;
+    endpoint = '/' + endpoint
   }
 
-  const url = `https://medium.com${endpoint}${endpoint.indexOf('?') === -1  ? '?' : '&' }format=json`;
+  const url = `https://medium.com${endpoint}`
 
-  return new Promise((resolve, reject) => https.get(url, res => {
-    if (callback && callback.write instanceof Function) {
-      return res.pipe(callback);
+  const rssToJsonResponse = await new Promise(resolve => Feed.load(url, (err, rss) => resolve(rss), (rawObject) => {
+    return {
+      ...(rawObject['content:encoded'] && {
+        contentEncoded: rawObject['content:encoded'][0]
+      })
     }
+  }))
 
-    res.statusCode === 200 || fail(res.statusCode, res.statusMessage, reject, callback);
-
-    let data = '';
-
-    res.on('data', chunk => (data += chunk));
-
-    res.on('end', () => {
-      try {
-        data = data.substr(data.indexOf('{'));
-        data = JSON.parse(data) || {};
-
-        const posts = data.payload && data.payload.posts
-          || data.payload && data.payload.references
-          && data.payload.references.Post
-          && Object.keys(data.payload.references.Post)
-            .map(key => data.payload.references.Post[key]);
-
-        if (posts) {
-          const result = { status: 200, response: posts };
-          callback instanceof Function && callback(result);
-          resolve(result);
-        } else {
-          fail(500, 'Could not parse the resource. Medium\'s JSON format might have changed.', reject, callback);
-        }
-
-      } catch (error) {
-        fail(500, error.message, reject, callback);
-      }
-    });
-  }).on('error', error => fail(500, error.message, reject, callback)));
-};
+  const result = {status: 200, response: rssToJsonResponse}
+  callback instanceof Function && callback(result)
+}
